f4172cbc0ec32e3540a45d639a1e125f73fdc115
mohit tomar <mohittomar13@gmail.com>
2022-09-11 11:28:29 +0530

    Completed the front-end of the application

--- Simplilearn_Assessment_1/src/simplilearn/assessments/LockedMe.java
+++ Simplilearn_Assessment_1/src/simplilearn/assessments/LockedMe.java
@@ -2,6 +2,15 @@ package simplilearn.assessments;
 
 public class LockedMe {
 	public static void main(String[] args) {
-		System.out.println("Test");
+		System.out.println("***************************");
+		System.out.println("    WELCOME TO LOCKEDME");
+		System.out.println("      by: Mohit Tomar");
+		System.out.println("***************************");
+		System.out.println("\n   MENU");
+		System.out.println("\t1) List files and directories");
+		System.out.println("\t2) Add files and directories");
+		System.out.println("\t3) Delete files and directories");
+		System.out.println("\t4) Search files and directories");
+		System.out.println("\t4) Quit");
 	}
 }
